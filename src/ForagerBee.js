1e0e6869c31f8acd8f3dcf8a4f24593a8b3b5bab
scurry222 <scurry222@gmail.com>
2020-11-30 11:01:14 -0800

    Add fleet of bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = this.color;
+    this.food = this.food;
+    this.canFly = true;
+    this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+    this.treasureChest.push(treasure);
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'make honey';
+    this.color = this.color;
+    this.food = this.food;
+    this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+    this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+    if (this.honeyPot > 0) {
+        this.honeyPot -= 1;
+    }
+};
